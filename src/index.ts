a961c1a97234ce69684e657b364f7e8dcae16282
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2021-07-07 02:25:58 +0530

    feat: adds /verify-certificate api using fastify
    asdf

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-function testVaccineVerification() {}
+import fastify from "fastify";
+import fastifyMultipart from "fastify-multipart";
 
-const vaccinationTestResult = testVaccineVerification();
-console.log(vaccinationTestResult);
+import { scanVaccineQr } from "./verifyCertificate";
+import { getImageDataFromMultipartFile } from "./utils";
+
+const server = fastify();
+server.register(fastifyMultipart);
+
+server.post("/verify-certificate", async (request) => {
+  const imageFile = await request.file();
+  const imageData = await getImageDataFromMultipartFile(imageFile);
+  const vaccineData = await scanVaccineQr(imageData);
+  return { data: vaccineData };
+});
+
+// TODO - move PORT to utils/constants
+server.listen(process.env.PORT || 8080, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.info(`Server listening at ${address}`);
+});
