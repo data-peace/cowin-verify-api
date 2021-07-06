3a53bb97a41d53d79e5c8019df09c294d7177752
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2021-07-06 18:17:45 +0530

    feat: adds utils for verifying certificate
    updates yarn.lock

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module "jsonld-signatures";
+declare module "crypto-ld";
+declare module "jsonld";
+declare module "security-context";
+declare module "vaccination-context";
---
+++ src/verifyCertificate/scanVaccineQr.ts
@@ -0,0 +1,35 @@
+import JSZip from "jszip";
+import { scanImageData } from "zbar.wasm";
+
+import type { CertificateData } from "./CertificateData";
+
+const CERTIFICATE_FILENAME = "certificate.json";
+
+async function getVaccineCertificateContent(data: string) {
+  if (!data) return "";
+
+  try {
+    const zip = new JSZip();
+    const contents = await zip.loadAsync(data);
+    const certificateContent = await contents.files[CERTIFICATE_FILENAME].async(
+      "text"
+    );
+    return JSON.parse(certificateContent) as CertificateData;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+export async function scanVaccineQr(qrImage: ImageData) {
+  const symbols = await scanImageData(qrImage);
+  for (let i = 0; i < symbols.length; ++i) {
+    const sym = symbols[i];
+    const certificateResult = await getVaccineCertificateContent(sym.decode());
+    if (certificateResult) {
+      return certificateResult;
+    }
+  }
+
+  return null;
+}
