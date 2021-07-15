ee4c9e1fb3a2657ac647f469579713828f29c7e4
Akshay Pratap Singh <pratapakshay0@gmail.com>
2021-07-15 14:20:29 +0530

    Remove port from pm2 ecosystem file

--- pm2-ecosystem.config.js
+++ pm2-ecosystem.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-    apps:[
-        {
-            name    : "cowin-verify-api",
-            script  : "dist/index.js",
-            env: {
-                PORT: 8080
-            },
-            env_production: {
-                NODE_ENV: "production",
-            },
-            env_development: {
-                NODE_ENV: "development",
-                PORT: 8000
-            }
-        }
-    ]
+  apps: [
+    {
+      name: "cowin-verify-api",
+      script: "dist/index.js",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_development: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
